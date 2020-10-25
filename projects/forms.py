d0c05000520b6135bb68ceef65dbd4dd23c5c1cb
albert obwoge <nyambegaobwoge@yahoo.com>
2020-10-25 13:49:36 +0300

    created a ratingform

--- projects/forms.py
+++ projects/forms.py
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+class RatingForm(forms.ModelForm):
+
+    class Meta:
+        model = Rating
+        fields = ['design', 'usability', 'content']
