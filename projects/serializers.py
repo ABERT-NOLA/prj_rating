e946aadb10e751841715041552274f290f6e452b
albert obwoge <nyambegaobwoge@yahoo.com>
2020-10-25 13:51:43 +0300

    added a postserializer for athe api endpoints

--- projects/serializers.py
+++ projects/serializers.py
@@ -5,3 +5,9 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'    
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True, required=False)
+    class Meta:
+        model = Post
+        fields = '__all__'
